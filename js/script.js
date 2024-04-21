a2811c217ac7a8ded5e049d94ee788e1426ee93d
niomion <nio.niomion@gmail.com>
2024-04-21 12:57:11 +0300

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-setInterval(showSlides, 4500); 
+
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
@@ -48,16 +48,19 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides() {
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 } 
-
-// maps
 
